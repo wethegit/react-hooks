68bc1372602daf39b613ad46900e35e170640cb3
Andrew Rubin <arubin78@gmail.com>
2022-12-23 16:32:41 -0800

    feat: add useInView hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { useAsync } from "./hooks/use-async"
+import { useInView } from "./hooks/use-in-view"
 import { useLocalStorage } from "./hooks/use-local-storage"
 import { useUserPrefs } from "./hooks/use-user-prefs"
 import { UserPreferencesProvider } from "./contexts/user-prefs-context"
 
-export { useAsync, useLocalStorage, useUserPrefs, UserPreferencesProvider }
+export { useAsync, useInView, useLocalStorage, useUserPrefs, UserPreferencesProvider }
