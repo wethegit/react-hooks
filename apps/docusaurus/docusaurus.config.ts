055b1dfec6f697ed8351f5cad60ca3dfdd2d505a
Marlon Marcello <marlon.marcello@gmail.com>
2023-11-28 13:33:27 -0800

    fix: package version

--- apps/docusaurus/docusaurus.config.ts
+++ apps/docusaurus/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types"
 import type * as Preset from "@docusaurus/preset-classic"
 
 const config: Config = {
-  title: "My Site",
+  title: "@wethegit/react-hooks",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
